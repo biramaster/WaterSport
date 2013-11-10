44467127211bb60df975cda6baf17a976e1ad30a
biramaster001 <biramaster001@200.168.0.114>
2013-11-10 16:36:45 +0100

    Initiate Interface for Equipaments

---
+++ JavaWaterSport01/src/javawatersport01/IEquipaments.java
@@ -0,0 +1,15 @@
+package javawatersport;
+
+/**
+ *
+ * @author André Rubira at biramaster001
+ * @version 2013.11.10
+ */
+public interface IEquipaments {
+    public String getId();
+    public void setId(String id);
+    public String getName();
+    public void setName(String name); 
+    
+    
+}
